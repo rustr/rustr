fb37479d898cd4de4c4d421539f6cfd8351f9f14
qinwf <mail@qinwenfeng.com>
2016-03-23 14:38:26 +0800

    Update: unsafe examples

--- examples/init.rs
+++ examples/init.rs
@@ -1,14 +1,12 @@
 #[macro_use]
 extern crate rustr;
 
-extern crate libc;
-
 use std::ffi::CString;
-use libc::{c_char,c_int};
+use std::os::raw::{c_char,c_int};
 use rustr::rdll::*;
 use std::iter::*;
-use rustr::protect::shield::*;
-use rustr::rdefined::*;
+use rustr::protect::stackp::*;
+use rustr::rtype::*;
 
 
 fn main(){
@@ -20,7 +18,7 @@ fn main(){
 
 	let d = Rf_initEmbeddedR(dd as c_int,args.as_ptr() as *mut *mut c_char);
 	let fun = Shield::new(Rf_findFun(Rf_install(cstring!("print")),  R_GlobalEnv));
-	let sdd = Shield::new(Rf_allocVector(INTSXP as SEXPTYPE,10));
+	let sdd = Shield::new(Rf_allocVector(INTSXP,10));
 	println!("{:?}", "here");
 	Rf_endEmbeddedR(0);
 	println!("{:?}", d);
