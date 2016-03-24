b2d817a8e350711cdec3f6912616d2b18a855964
qinwf <mail@qinwenfeng.com>
2016-03-24 17:55:04 +0800

    Format: engine code

--- src/feature/engine.rs
+++ src/feature/engine.rs
@@ -29,6 +29,11 @@ impl REngine {
     }
     pub fn initialize(args: Vec<CString>, verbose: bool, interactive: bool) -> RResult<Self> {
         let dd = args.len();
+        unsafe {
+            if !REng.is_null() {
+                return rerror(REKind::BindingIsLocked("there is a runing R engine".into()));
+            }
+        }
         let args: Vec<*mut c_char> = args.into_iter()
                                          .map(|arg| arg.as_ptr() as *mut c_char)
                                          .collect();
@@ -38,15 +43,15 @@ impl REngine {
                 R_SignalHandlers = 0;
             }
         }
-        
+
         // init_tempdir?
-        
+
         if let Err(_) = env::var("R_HOME") {
             unsafe {
                 let xs: String = try!(CStr::from_ptr(get_R_HOME()).to_owned().into_string());
-            	env::set_var("R_HOME", &xs)
+                env::set_var("R_HOME", &xs)
             }
-            
+
         }
         let d = unsafe { Rf_initEmbeddedR(dd as c_int, args.as_ptr() as *mut *mut c_char) };
 
@@ -55,25 +60,37 @@ impl REngine {
                 R_CStackLimit = ::std::os::raw::c_ulong::max_value();
             }
         }
-        
-                if d != 0 {
+
+        if d != 0 {
             return rraise("can not create REngine.");
         }
-        
-		unsafe {R_ReplDLLinit();} // populate the repl console buffers
-		
 
-        
+        unsafe {
+            R_ReplDLLinit();
+        } // populate the repl console buffers
+
+
+
         let mut rst = structRstart::default();
-        let startr :* mut structRstart = &mut rst;
-    	unsafe {
-    		R_DefParams( startr);
-    	}
-    	
-		rst.R_Interactive = if interactive {Rboolean::TRUE} else {Rboolean::FALSE};       // sets interactive() to eval to false
-		rst.R_Verbose = if verbose {Rboolean::TRUE} else {Rboolean::FALSE}; 
-		
-        unsafe {R_SetParams(startr);}
+        let startr: *mut structRstart = &mut rst;
+        unsafe {
+            R_DefParams(startr);
+        }
+
+        rst.R_Interactive = if interactive {
+            Rboolean::TRUE
+        } else {
+            Rboolean::FALSE
+        };       // sets interactive() to eval to false
+        rst.R_Verbose = if verbose {
+            Rboolean::TRUE
+        } else {
+            Rboolean::FALSE
+        };
+
+        unsafe {
+            R_SetParams(startr);
+        }
         let envir = EnvirN::global();
         let mut res = REngine {
             verbose_m: verbose,
@@ -81,8 +98,10 @@ impl REngine {
             global_env: envir,
             code: "".into(),
         };
-        
-		unsafe {REng = &mut res;}
+
+        unsafe {
+            REng = &mut res;
+        }
         Ok(res)
     }
 }
