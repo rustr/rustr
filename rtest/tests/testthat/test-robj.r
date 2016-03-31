ce9300de321bf1f16752f7bf4710b9b7eea59d62
qinwf <mail@qinwenfeng.com>
2016-04-01 00:44:00 +0800

    Add: NumVec tests

--- rtest/tests/testthat/test-robj.r
+++ rtest/tests/testthat/test-robj.r
@@ -15,3 +15,9 @@ expect_equal(c1, rtest:::ucharvec())
 expect_equal(c1, rtest:::charvec())
 expect_equal(c2, rtest:::uncharvec())
 expect_equal(c2, rtest:::ncharvec())
+
+expect_equal( rtest:::charvec_at(c("sd","sS","sa","as","sdsd","sdsss"),0L),"sd")
+expect_error(rtest:::charvec_at(c("sd","sS","sa","as","sdsd","sdsss"),11L))
+
+expect_equal( rtest:::numvec_at(c(0,1,2,3),3L),3)
+expect_equal(0, rtest:::numvec_at(c(5,1,2,3),4L))
